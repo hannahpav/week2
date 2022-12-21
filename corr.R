38c135d91be08b236414fdc2e3919496908b523d
hannahpav <hannah.pavlovich@gmail.com>
2022-12-21 18:11:10 +0100

    nitrate and sulfate still not matching, made a complete_thr

--- corr.R
+++ corr.R
@@ -3,7 +3,7 @@ corr <- function(directory, threshold = 0) {
       ##Read Files in ID
       
       files332 <- list.files(directory)
-      readfiles <- lapply(file.path(directory, files332), read.csv)
+     # readfiles <- lapply(file.path(directory, files332), read.csv)
       
       files_run <- NULL
       corr_plan <- NULL
@@ -11,18 +11,14 @@ corr <- function(directory, threshold = 0) {
       source("complete.R")
       
       complete_ind <- complete(directory)
+      complete_thr <- complete_ind[complete_ind$nobs>=threshold,]
       
-      for(i in 1:length(files332)) {
+      for(i in complete_thr$id) {
             
-            if((complete_ind$nobs[i]) > threshold) {
-                 files_run <- c(files_run, i)}
-                 }
-      
-     # files_run}
-      
-      for(j in (files_run)) {
-            dftemp <- readfiles[[j]]
-           
+            readfiles <- lapply(file.path(directory, files332[i]), read.csv)
+            
+            dftemp <- readfiles[[1]]
+            
             whole_sulfate <- dftemp[["sulfate"]]
             real_sulfate <- whole_sulfate[!is.na(whole_sulfate)]
             
@@ -31,8 +27,23 @@ corr <- function(directory, threshold = 0) {
             
             corr_each <- cor(real_sulfate, real_nitrate)
             corr_plan <- c(corr_plan, corr_each)
-      }
+                 }
       
+     # files_run}
+#       
+#       for(j in (files_run)) {
+#             dftemp <- readfiles[[j]]
+#            
+#             whole_sulfate <- dftemp[["sulfate"]]
+#             real_sulfate <- whole_sulfate[!is.na(whole_sulfate)]
+#             
+#             whole_nitrate <- dftemp[["nitrate"]]
+#             real_nitrate <- whole_nitrate[!is.na(whole_nitrate)]
+#             
+#             corr_each <- cor(real_sulfate, real_nitrate)
+#             corr_plan <- c(corr_plan, corr_each)
+#       }
+#       
       corr_plan
-      
+
 }
