a0ae133e6e3e6e8ac27f5b0913593b8454de3a34
hannahpav <hannah.pavlovich@gmail.com>
2022-12-21 17:10:49 +0100

    first pas at for/if loop

--- corr.R
+++ corr.R
@@ -8,36 +8,16 @@ corr <- function(directory, threshold = 0) {
       files_run <- NULL
       corr_plan <- NULL
       
+      source("complete.R")
       
-      for(i in 1:length(files332) {
+      for(i in 1:length(files332)) {
             
-            if()
+            complete_ind <- complete(directory)
+            
+            if((complete_ind$nobs[i]) > threshold)
+                  {files_run <- c(files_run, i}
+                  
+      }
+      files_run
       }
       
-      complete <- function(directory, id = 1:332) {
-            
-            ##Read Files in ID
-            
-            files332 <- list.files(directory)
-            readfiles <- lapply(file.path(directory, files332), read.csv)
-            
-            # create empty vecotor
-            
-            nobs <- NULL
-            
-            # loop to read in data  
-            for(i in id) {
-                  
-                  dfttemp <- readfiles[[i]] # read the files into a temp frame
-                  nobs_sum <- sum(complete.cases(dfttemp)) # some of the complete
-                  
-                  nobs <- c(nobs, nobs_sum) #create a vector of all "nobs"
-                  
-                  
-            }
-            
-            data.frame(id, nobs) #create data.frame
-      }
-      
-      
-}
