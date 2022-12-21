f6c6ae0254dd1254a8be42cba579ac264fe876b0
hannahpav <hannah.pavlovich@gmail.com>
2022-12-21 17:19:31 +0100

    first pass at adding the corr plan

--- corr.R
+++ corr.R
@@ -10,14 +10,23 @@ corr <- function(directory, threshold = 0) {
       
       source("complete.R")
       
+      complete_ind <- complete(directory)
+      
       for(i in 1:length(files332)) {
             
-            complete_ind <- complete(directory)
-            
-            if((complete_ind$nobs[i]) > threshold)
-                  {files_run <- c(files_run, i}
-                  
-      }
-      files_run
+            if((complete_ind$nobs[i]) > threshold) {
+                 files_run <- c(files_run, i)}
+                 }
+      
+      #print(files_run)
+      
+      for(i in (files_run)) {
+            dftemp <- readfiles[[i]]
+            corr_each <- cor(dftemp["sulfate"], dftemp["nitrate"])
+            corr_plan <- c(corr_plan, corr_each)
       }
       
+      coor_plan
+      
+}
+
