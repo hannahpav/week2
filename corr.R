99cf6014d541da059b1bc0237e003c606d3be0a1
hannahpav <hannah.pavlovich@gmail.com>
2022-12-21 16:45:52 +0100

    first lines of code

--- corr.R
+++ corr.R
@@ -5,4 +5,39 @@ corr <- function(directory, threshold = 0) {
       files332 <- list.files(directory)
       readfiles <- lapply(file.path(directory, files332), read.csv)
       
+      files_run <- NULL
+      corr_plan <- NULL
+      
+      
+      for(i in 1:length(files332) {
+            
+            if()
+      }
+      
+      complete <- function(directory, id = 1:332) {
+            
+            ##Read Files in ID
+            
+            files332 <- list.files(directory)
+            readfiles <- lapply(file.path(directory, files332), read.csv)
+            
+            # create empty vecotor
+            
+            nobs <- NULL
+            
+            # loop to read in data  
+            for(i in id) {
+                  
+                  dfttemp <- readfiles[[i]] # read the files into a temp frame
+                  nobs_sum <- sum(complete.cases(dfttemp)) # some of the complete
+                  
+                  nobs <- c(nobs, nobs_sum) #create a vector of all "nobs"
+                  
+                  
+            }
+            
+            data.frame(id, nobs) #create data.frame
+      }
+      
+      
 }
