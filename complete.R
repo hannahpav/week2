7ea4a119a1dc687ff13e6f599de5afad2dc5e16f
hannahpav <hannah.pavlovich@gmail.com>
2022-12-21 15:27:03 +0100

    I hope I did this right

--- complete.R
+++ complete.R
@@ -5,46 +5,20 @@ complete <- function(directory, id = 1:332) {
        files332 <- list.files(directory)
        readfiles <- lapply(file.path(directory, files332), read.csv)
        
-       # create empty dataframe
+       # create empty vecotor
        
-      data <- data.frame(bozo = rep(NA, length(id)), nobs=NA)
-        
-      #nods_length <- integer(length(id))
+      nobs <- NULL
           
-            #  # loop to read in data  
+            # loop to read in data  
             for(i in id) {
-                  dfttemp <- readfiles[[i]]
-                  nobs_sum <- sum(complete.cases(dfttemp))
-
-                 data$nobs[seq_along(id[i])] <- nobs_sum
-            #       
-            #      #data$id[i] <- i
+                  
+                  dfttemp <- readfiles[[i]] # read the files into a temp frame
+                  nobs_sum <- sum(complete.cases(dfttemp)) # some of the complete
+                  
+                 nobs <- c(nobs, nobs_sum) #create a vector of all "nobs"
+             
+                  
              }
        
-       print(data)
+       data.frame(id, nobs) #create data.frame
 }
-      
-     # for(i in id) {
-     #         dfttemp <- readfiles[[i]]
-     #         nobs_sum <- sum(complete.cases(readfiles[[i]]))
-     # 
-     #         #data$nobs[i] <- nobs_sum
-     # 
-     #         data$id[i] <- i
-     # 
-     #   }
-#       
-#       print(data)
-# }
-
-            
-      # data.frame(id, sumtrue)
-      # sum_true
-            
-      #       ## calculate the number of items to mean
-      #       sum_placeholder <- is.na(dftemp[pollutant])
-      #       sum_placeholder_inv <- !sum_placeholder
-      #       sum_true <- sum_true + sum(sum_placeholder_inv)}
-      # 
-      # ## calculate mean
-      # totalsum/sum_true
